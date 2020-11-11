7ddaac2582d3ba5e4e6dc008682aef4e4e71250a
Shahriar220 <shahriar.uchchash@gmail.com>
2020-11-11 23:10:10 +0600

    reset password added

--- models/user.js
+++ models/user.js
@@ -11,6 +11,8 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    resetToken: String,
+    resetTokenExpiration: Date,
     cart: {
         items: [{
             productId: { type: Schema.Types.ObjectID, ref: 'Product', required: true },
